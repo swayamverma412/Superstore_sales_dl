b6ec047170e8e4ec97091b02e367b53a7d0b6923
swayamverma412 <swayamverma412@gmail.com>
2023-04-18 03:26:11 +0530

    api udated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # Load the machine learning model
 model = tf.keras.models.load_model('predict.h5')
-
+# model.predict([[88.0,9,'Category_Furniture']])
 
 @app.route('/')
 def home():
@@ -21,18 +21,25 @@ def predict():
  Profit = float(request.form['profit'])
  Quantity = int(request.form['quantity'])
  Category= request.form['category']
-    
+ print(Profit, Quantity, Category)
      # Process the input data
  Category = Category.strip().title()
-
- if Category not in ['Category_Furniture', 'Category_Office Supplies', 'Category_Technology']:
+ data=["Category_Furniture", "Category_Office Supplies", "Category_Technology"]
+ try:
+    Category=data.index(Category)
+ except Exception as e:
+    print(e)
     return "Invalid category. Please enter a valid category (Furniture/Office Supplies/Technology)."
-
- 
- prediction = model.predict([[Profit, Quantity, Category]])
-    
- return render_template('index.html', prediction_text=f"The predicted sales value is {prediction[0]:,.2f}")
-
+ try:
+    prediction = model.predict([[Profit, Quantity, Category]])
+    # return ""
+    prediction=prediction[0][0]
+    # return ''
+    return render_template('index.html', prediction_text="The predicted sales value is %.2f"%prediction)
+ except Exception as e:
+    print(e)
+    print(Profit, Quantity, Category)
+    return "Invalid input. Please enter a valid input."
 
 if __name__ == "__main__":
     app.run(debug=True)
